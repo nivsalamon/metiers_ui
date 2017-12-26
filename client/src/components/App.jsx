c1ca9af8eb0d8e3e0d64abeea401aa7e6e5829a0
Niv Salamon <mnsalamon@gmail.com>
2017-12-26 03:21:16 -0800

    basic navigation and browser routing set up

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navigation from './Navigation/Navigation';
+import Login from './Auth/Login';
+import Signup from './Auth/Signup';
+import Home from './Homepage/Dashboard/index';
+
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Inside App</h1>
-      </div>
+              <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/home" component={Home} />
+              </Switch>
     );
   }
 }
 
 
 export default App;
-
---
+++ client/src/components/Auth/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+import { Link, Redirect } from 'react-router-dom';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div>
+        Login Page
+      </div>
+    );
+  }
+}
+export default Login;
