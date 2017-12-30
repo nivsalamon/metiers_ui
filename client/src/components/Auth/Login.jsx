199d74791397371cfca6ca39db1dab8323bc7a00
Niv Salamon <mnsalamon@gmail.com>
2017-12-29 17:16:41 -0800

    front-end handles 200 and 401 responses for email in DB or not

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -5,7 +5,38 @@ import { Link, Redirect } from 'react-router-dom';
 class Login extends React.Component {
   constructor() {
     super();
+    this.state = {
+      email: '',
+      password: '',
+    }
+    this.loginSubmit = this.loginSubmit.bind(this);
+  }
 
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+    //console.log(this.state)
+  }
+
+
+
+
+
+  loginSubmit(e) {
+    e.preventDefault();
+    axios.post('http://localhost:3003/login', {
+      email: this.state.email,
+      password: this.state.password,
+    }).then((res) => {
+      console.log('res FE', res);
+      if(res.status === 200){
+        // this.setState({signedup: true});
+        console.log('nice worked!!!')
+      } 
+    }).catch(err => {
+      if(err.response.status === 401) {
+        alert('Please login again. Email or Password is incorrect.');
+      }
+    })
   }
 
   render() {
@@ -14,10 +45,10 @@ class Login extends React.Component {
         <div className="row justify-content-md-center">
           <div className="col-md-5">
             <label>Login</label>
-            <input placeholder="Login" className="form-control"/>
+            <input placeholder="Email" className="form-control" name="email" onChange={this.handleChange.bind(this)}/>
             <label>Password</label>
-            <input placeholder="Password" className="form-control" type="password"/>
-            <button className="btn btn-primary push-top-sm-xs">Login</button>
+            <input placeholder="Password" className="form-control" type="password" name="password" onChange={this.handleChange.bind(this)}/>
+            <button className="btn btn-primary push-top-sm-xs" onClick={this.loginSubmit}>Login</button>
             <Link to="/signup" href="/signup" className="btn btn-info push-top-sm-xs push-left-sm">Sign Up</Link>
           </div>
         </div>
--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -13,7 +13,6 @@ class Signup extends React.Component {
         signedup: false
     }
     this.handleChange = this.handleChange.bind(this);
-    this.signUpSubmit = this.signUpSubmit.bind(this);
   }
 
   handleChange(e) {
